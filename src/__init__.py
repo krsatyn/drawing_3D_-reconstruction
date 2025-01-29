0034f7447dccf5f1f8b18189c0493cc24c4ba93f
Krsatyn <92187267+krsatyn@users.noreply.github.com>
2025-01-29 12:08:01 +0400

    upd __init__

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+__all__ = ["config",
+           "process",
+           "run_notebook",
+           "train_model"]
+
+from . import config
+from . import process
+from . import run_notebook
+from . import train_model
